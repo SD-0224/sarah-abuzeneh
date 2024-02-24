19e91e96be1d1464b6eb23fcc6d4912e101e6c6e
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2024-02-24 15:46:28 +0200

    Add Favourite style

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const modeToggleBtn = document.getElementById('mode-toggle');
+const favoriteToggleBtn = document.getElementById('favorite-toggle');
+const favoritesArticlesContainer = document.getElementsByClassName('favorites-articles-wrapper')[0];
 
 modeToggleBtn.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
+});
+
+favoriteToggleBtn.addEventListener('click', () => {
+    if (favoritesArticlesContainer.style.display === 'none') {
+        favoritesArticlesContainer.style.display = 'flex';
+    } else {
+        favoritesArticlesContainer.style.display = 'none';
+    }
 });
